812428ebf36816b4b61dce821f7011936848c0d0
Peter Gu <peterouwen@yeah.net>
2019-04-12 23:51:38 -0700

    Add files via upload

---
+++ Flixter/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flixter
+//
+//  Created by Peter Gu on 4/12/19.
+//  Copyright © 2019 Peter Gu. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
