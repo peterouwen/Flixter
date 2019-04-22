b38a1848fafc223510a852d333878df4e55608e5
Pingde Gu <peterouwen@gmail.om>
2019-04-21 17:59:59 -0700

    Part 2

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Peter Gu on 4/21/19.
+//  Copyright © 2019 Peter Gu. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
